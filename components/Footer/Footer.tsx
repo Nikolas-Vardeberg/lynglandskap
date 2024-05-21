e8b1fcc937bcc6b42934a2c42adc4925bf0b402b
niklusrook <post@nikolas.no>
2024-05-21 14:33:49 +0200

    feat: mediaModule image fix h

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,6 +3,10 @@ import * as React from 'react'
 
 import { FooterLogo } from './FooterLogo'
 
+import { FacebookIcon, InstagramIcon } from 'lucide-react';
+
+import { CopyrightIcon } from 'lucide-react'
+
 type FooterLink = {
   label: string
   url: string
@@ -16,20 +20,23 @@ export type FooterProps = {
   privacyPolicy: FooterLink
 }
 
+
 export type Props = FooterProps
 
-export const Footer = ({ copyright, socialLinks, privacyPolicy }: Props) => {
+export const Footer = ({ copyright, socialLinks, privacyPolicy, menu }: Props) => {
+
+  console.log(socialLinks)
   return (
-    <footer className="bg-primary-700 py-8 md:py-16">
-      <div className="container mx-auto px-5 text-white">
+    <footer className="bg-white border-t  py-8 md:py-16">
+      <div className="container mx-auto px-5 text-black">
         <div>
-          <Link href={'/'}>
-            <FooterLogo />
+          <Link href={'/'} className='font-bold text-2xl'>
+            Logo
           </Link>
         </div>
         <div className="flex flex-col-reverse gap-12 pt-12 md:flex-row md:items-center md:justify-between md:pt-8">
           <div className="flex flex-col gap-2 md:flex-row md:gap-4 ">
-            {copyright && <span>{copyright}</span>}
+            <CopyrightIcon /> {copyright && <span>{copyright}</span>}
             {privacyPolicy && (
               <Link
                 className="underline hover:no-underline"
@@ -40,20 +47,42 @@ export const Footer = ({ copyright, socialLinks, privacyPolicy }: Props) => {
             )}
           </div>
           {socialLinks && (
-            <ul className="ml-0 flex flex-wrap gap-4 md:flex-row ">
-              {socialLinks.map((link) => {
-                return (
-                  <li key={link.url}>
-                    <Link
-                      className="border-b-2 border-transparent font-semibold text-white hover:border-white"
-                      href={link.url}
-                    >
-                      {link.label}
-                    </Link>
-                  </li>
-                )
-              })}
-            </ul>
+           <ul className="ml-0 flex flex-wrap gap-4 md:flex-row space-x-4">
+           {socialLinks.map((link) => {
+             let IconComponent;
+             switch (link.label.toLowerCase()) {
+               case 'facebook':
+                 IconComponent = FacebookIcon;
+                 break;
+               case 'instagram':
+                 IconComponent = InstagramIcon;
+                 break;
+               default:
+                 return (
+                   <li key={link.url}>
+                     <Link
+                       className="border-transparent font-semibold text-black "
+                       href={link.url}
+                     >
+                       {link.label}
+                     </Link>
+                   </li>
+                 );
+             }
+       
+             return (
+               <li key={link.url}>
+                 <Link
+                   className="font-semibold text-black  flex"
+                   href={link.url}
+                 >
+                    <IconComponent size={24} />
+                    {link.label}
+                 </Link>
+               </li>
+             );
+           })}
+         </ul>
           )}
         </div>
       </div>
--- components/MediaModule/MediaModule.tsx
+++ components/MediaModule/MediaModule.tsx
@@ -25,33 +25,27 @@ export const MediaModule = ({
   isDark = false,
 }: Props) => {
   return (
-    <div className={isDark ? 'bg-primary-200' : 'bg-grey-100'}>
-      <div className="container mx-auto">
-        <div className="grid grid-cols-1 px-5 py-12 sm:gap-x-5 md:grid-cols-2 md:py-20">
+    <div className={isDark ? 'bg-green-600' : 'bg-white'}>
+      <div className="container mx-auto py-48">
+        <div className="grid xl:grid-cols-2 gap-12 md:gap-36">
           <div
-            className={`relative mx-auto mb-6 mt-10 h-64 w-64 self-start sm:h-72 sm:w-72 md:mt-0 ${
+            className={`relative mx-auto self-start w-full h-[450px] sm:h-[700px] bg-red-500 ${
               imageOnRight ? 'order-last mt-8' : ''
             }`}
           >
-            <div
-              className={`absolute inset-0 h-64 w-64 translate-x-3 transform ${
-                isDark ? 'bg-secondary-300' : 'bg-secondary-600'
-              } sm:h-72 sm:w-72`}
-            >
               <ImageBox
-                width={350}
-                height={350}
+                width={420}
+                height={520}
                 image={image}
                 alt={imageAlt}
-                className="absolute inset-0 h-64 w-64 -translate-x-6 translate-y-6 transform sm:h-72 sm:w-72"
+                className="absolute inset-0 w-full h-full object-cover transform"
               />
-            </div>
           </div>
           <div className="mt-10 flex flex-col justify-center md:mt-0">
             {heading && (
               <h2
-                className={`font-times text-2xl font-bold lg:text-3xl ${
-                  isDark ? 'text-grey-50' : 'text-primary-700'
+                className={`font-sans text-features leading-none font-bold ${
+                  isDark ? 'text-white' : 'text-black'
                 }`}
               >
                 {heading}
@@ -59,8 +53,8 @@ export const MediaModule = ({
             )}
             {body && (
               <div
-                className={`prose mt-6 text-base ${
-                  isDark ? 'text-grey-50' : 'text-primary-700'
+                className={`prose mt-6 ${
+                  isDark ? 'text-white/80' : 'text-black/80'
                 }`}
               >
                 {body}
