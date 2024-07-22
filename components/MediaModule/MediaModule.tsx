a26b76e7c5a30feaca4c53a95212dfc442d5f091
Nikolas, Vardeberg - Rek <nikolas.vardeberg1@gmail.com>
2024-07-23 00:35:15 +0200

    feat: color change + input and submit button

--- components/MediaModule/MediaModule.tsx
+++ components/MediaModule/MediaModule.tsx
@@ -26,7 +26,7 @@ export const MediaModule = ({
   isDark = false,
 }: Props) => {
   return (
-    <div className={isDark ? 'bg-green-600' : 'bg-[#EBF2FA]'}>
+    <div className={isDark ? 'bg-green-600' : 'bg-[#E9F7DB]'}>
       <div className='grid grid-cols-1 md:grid-cols-2 '>
         <div className='flex flex-col justify-between h-screen w-full text-center md:text-left py-12 px-12 md:py-20 md:container'>
             <div>
@@ -41,7 +41,11 @@ export const MediaModule = ({
               </div>
               <div className='mt-28 space-y-8'>
                 <h1 className='text-4xl text-black'>{heading}</h1>
-                <h2 className='text-lg text-gray-500'>{body}</h2>
+                <h2 className='text-lg text-gray-600'>{body}</h2>
+                  <div className='flex flex-col gap-[26px] items-center w-full md:items-start'>
+                    <input type="email" name="email" required className="p-3 w-full rounded-[8px] bg-transparent border border-[#828282] max-w-[400px]" placeholder="e-post"/>
+                    <button type="submit" className="group w-full relative inline-flex h-14 items-center justify-center overflow-hidden rounded-md bg-[#97A97C] px-6 font-medium text-white max-w-[400px] duration-500"><div className="translate-x-0 opacity-100 transition group-hover:-translate-x-[150%] group-hover:opacity-0">Kontakt Oss</div><div className="absolute translate-x-[150%] opacity-0 transition group-hover:translate-x-0 group-hover:opacity-100"><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6"><path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg></div></button>  
+                  </div>
               </div>
             </div>
             <div className='text-base md:text-left text-center md:text-base text-gray-500'>
