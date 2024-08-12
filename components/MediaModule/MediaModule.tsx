da8b34aa1fb956c3382c227e7519e87683c8d206
niklusrook <post@nikolas.no>
2024-08-12 21:14:08 +0200

    update: media module text pallet

--- components/MediaModule/MediaModule.tsx
+++ components/MediaModule/MediaModule.tsx
@@ -3,6 +3,7 @@ import { Cta } from '../Cta/Cta'
 
 import ImageBox from 'components/shared/ImageBox'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export type MediaModule = {
   body?: string
@@ -40,9 +41,9 @@ export const MediaModule = ({
               />
               </div>
               <div className='mt-28 space-y-8'>
-                <h1 className={`text-4xl ${isDark ? 'text-black': 'text-black'}`}>{heading}</h1>
-                <h2 className={`text-lg font-light ${isDark ? 'text-black': 'text-black'}`}>{body}</h2>
-                <button type="submit" className={`group w-full relative inline-flex h-14 items-center justify-center overflow-hidden rounded-md px-6 font-medium max-w-[400px] duration-500 ${isDark ? 'bg-white text-black' : 'bg-[#42A150] text-white'}`}><div className="translate-x-0 opacity-100 transition group-hover:-translate-x-[150%] group-hover:opacity-0">Kontakt Oss</div><div className="absolute translate-x-[150%] opacity-0 transition group-hover:translate-x-0 group-hover:opacity-100"><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6"><path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg></div></button>  
+                <h1 className={`text-[40px] ${isDark ? 'text-black': 'text-black'}`}>{heading}</h1>
+                <h2 className="text-[30px] text-gray-500">{body}</h2>
+                <Link href="/kontakt"  className={`group w-full relative inline-flex h-14 items-center justify-center overflow-hidden rounded-md px-6 font-medium max-w-[400px] duration-500 ${isDark ? 'bg-white text-black' : 'bg-[#42A150] text-white'}`}><div className="translate-x-0 opacity-100 transition group-hover:-translate-x-[150%] group-hover:opacity-0">Kontakt Oss</div><div className="absolute translate-x-[150%] opacity-0 transition group-hover:translate-x-0 group-hover:opacity-100"><svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6"><path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg></div></Link>  
               </div>
             </div>
             <div className='text-base md:text-left text-center md:text-base text-black'>
