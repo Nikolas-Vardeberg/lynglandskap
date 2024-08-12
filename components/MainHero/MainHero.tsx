80ca252bba8a40ad1704c55ee056d4e1bb94ec64
niklusrook <post@nikolas.no>
2024-08-12 16:28:57 +0200

    feat: Refactor MainHero component layout and styling

--- components/MainHero/MainHero.tsx
+++ components/MainHero/MainHero.tsx
@@ -2,14 +2,11 @@
 import ImageBox from 'components/shared/ImageBox'
 import * as React from 'react'
 
-import { Cta, CtaProps } from '../Cta/Cta'
-
 export type MainHero = {
   heading: string
   body?: string
   heroImage: any
   heroImageAlt: string
-  cta?: CtaProps
 }
 
 export type Props = MainHero
@@ -19,29 +16,30 @@ export const MainHero = ({
   body,
   heroImage,
   heroImageAlt,
-  cta,
 }: Props) => {
   return (
-    <div className='bg-[#f0f8ed] py-10 pt-20 px-20'>
-       <div className='text-center max-4xl mx-auto md:text-left space-y-8'>
-          <h2 className='text-[40px]'>
-            {heading}
-          </h2>
-          <h4 className='max-w-7xl text-[30px]'>
+    <div className='bg-[#f0f8ed] py-10 pt-20 px-8 md:px-20'>
+       <div className='max-4xl mx-auto space-y-8'>
+          {heading && (
+            <h1 className='text-[30px] md:text-[40px] text-black'>
+              {heading}
+            </h1>
+          )}
+        {body && (
+          <h4 className='max-w-7xl text-left text-black text-[20px]   md:text-[30px]'>
             {body}
           </h4>
+        )}
         </div>
-        <div className='w-full mt-12'>
-          {heroImage && (
-            <div>
-              <ImageBox
+        {heroImage && (
+          <div className='w-full mt-12 text-black'>
+             <ImageBox
                 image={heroImage}
                 alt={heroImageAlt}
                 className='md:w-full md:h-full '
               />
-            </div>
-          )}
-      </div>
+          </div>
+        )}
       </div>
   )
 }
