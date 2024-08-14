9a0ce9af8a74ab5801c395d1a9f6c75abdc11b93
Nikolas, Vardeberg - Rek <nikolas.vardeberg1@gmail.com>
2024-08-14 19:44:58 +0200

    feat: sitemap + robots

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://www.lynglandskap.no/',
+    generateRobotsTxt: true, // (optional) Generate robots.txt
+    // Optional additional options
+  };
+  
